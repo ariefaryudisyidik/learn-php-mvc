40dd80173ced7f39bb370a0e1f29a47f1b3c3eb9
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2024-05-05 16:34:12 +0700

    feat: Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,3 @@
 <h1>About Me</h1>
-<p>Hello my names is <?= $data['name']; ?>. I'm <?= $data['age']; ?> years old, I'am a <?= $data['job']; ?></p>
+<img src="<?= BASE_URL?>/img/profile.jpeg" alt="<?= $data['name']?>" width="200" class="rounded-circle">
+<p>Hello my names is <?= $data['name']?>. I'm <?= $data['age']?> years old, I'am a <?= $data['job']?></p>
