064b80e6fa604c4805e8714287e54e1169ea21ab
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2024-05-06 21:00:57 +0700

    feat: Flash Message

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -27,6 +27,11 @@ class StudentController extends Controller
     public function store()
     {
         if ($this->model('Student')->insertStudent($_POST) > 0) {
+            Flasher::setFlash('successfully', 'inserted', 'success');
+            header('Location: '.BASE_URL.'/student');
+            exit;
+        } else {
+            Flasher::setFlash('failed', 'inserted', 'danger');
             header('Location: '.BASE_URL.'/student');
             exit;
         }
